dfe9bc6d3e10170b7bc6f54db3846ee2a0fe4527
Muhammad Ikram Bin Muhammad Saifuddin <your.email@example.com>
2025-01-19 22:37:27 +0800

    Added files to S10267028_S10267420_PRG2Assignment_Flight Information Display System

---
+++ S10267028_S10267420_PRG2Assignment_Flight Information Display System/BoardingGate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10267028_S10267420_PRG2Assignment_Flight_Information_Display_System
+    {
+    internal class BoardingGate
+        {
+        }
+    }
